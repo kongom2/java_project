a56228e7d59302a98105d0a7418252a17c24bd42
admin <admin@YD5-14>
2023-08-08 11:06:45 +0900

    first commit

---
+++ H20230808/src/co/yedam/Hello.java
@@ -0,0 +1,7 @@
+package co.yedam;
+
+public class Hello {
+	public static void main(String[] args) { // 객체안에 있으므로 메서드
+		System.out.println("안녕하세요...");
+	} 
+}
---
+++ H20230808/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module H20230808 {
+	requires java.base; // 필요한 모듈 기술
+	requires java.desktop
+}
